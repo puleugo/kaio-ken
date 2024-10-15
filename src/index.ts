4d84033ad610bf85baadb2eee3b9444dc66da572
puleugo <puleugo@gmail.com>
2024-10-15 16:13:35 +0900

    feat: Register Git Action

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import * as core from '@actions/core';
 import {app} from "./app.js";
-app.generateOriginalPost();
 
+const methodObject = {
+	'READ': () => app.generateOriginalPost(),
+	'PUBLISH': () => new Promise<void>(resolve => resolve()),
+	'UPDATE_SPREAD_SHEEt': () =>app.updateSpreadSheetSettings(),
+}
+
+const method =  core.getInput('METHOD') as keyof typeof methodObject;
+const isMethodExist = methodObject.hasOwnProperty(method);
+if (!isMethodExist) {
+	core.setFailed('Invalid method');
+} else {
+	methodObject[method]();
+}
