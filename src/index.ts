b863935d1a2dc820e12c07d180245ec52b501386
puleugo <puleugo@gmail.com>
2024-11-12 18:25:21 +0900

    feat: 메서드 입력 디버거 추가

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import {app} from "./app";
 import {envManager} from "./util/config/env-manager";
 
 if (process.env.NODE_ENV !== 'test') {
-	envManager.putOrThrow('GH_REPOSITORY', core.getInput('GH_REPOSITORY'));
-	envManager.putOrThrow('GH_TOKEN', core.getInput('GH_TOKEN'));
-	envManager.putOrThrow('GH_USER', core.getInput('GH_USER'));
-	envManager.putOrThrow('GOOGLE_SHEET_ID', core.getInput('GOOGLE_SHEET_ID'));
-	envManager.putOrThrow('GOOGLE_CLIENT_EMAIL', core.getInput('GOOGLE_CLIENT_EMAIL'));
-	envManager.putOrThrow('GOOGLE_PRIVATE_KEY', core.getInput('GOOGLE_PRIVATE_KEY'));
+	envManager.putOrThrow('GH_REPOSITORY', core.getInput('GH_REPOSITORY', {required: true}));
+	envManager.putOrThrow('GH_TOKEN', core.getInput('GH_TOKEN', {required: true}));
+	envManager.putOrThrow('GH_USER', core.getInput('GH_USER', {required: true}));
+	envManager.putOrThrow('GOOGLE_SHEET_ID', core.getInput('GOOGLE_SHEET_ID', {required: true}));
+	envManager.putOrThrow('GOOGLE_CLIENT_EMAIL', core.getInput('GOOGLE_CLIENT_EMAIL', {required: true}));
+	envManager.putOrThrow('GOOGLE_PRIVATE_KEY', core.getInput('GOOGLE_PRIVATE_KEY', {required: true}));
 
 	envManager.put('MEDIUM_TOKEN', core.getInput('MEDIUM_TOKEN'));
 	envManager.put('OPENAI_API_KEY', core.getInput('OPENAI_API_KEY'));
@@ -20,12 +20,12 @@ async function bootstrap() {
 		'PUBLISH': () => app.uploadPosts(),
 		'UPDATE_SPREAD_SHEET': () =>app.updateSpreadSheetSettings(),
 	}
-	const methods =  core.getMultilineInput('METHOD') as Array<keyof typeof methodObject>
+	const methods =  core.getMultilineInput('METHOD', {required: true}) as Array<keyof typeof methodObject>
 	core.debug(`methods: ${methods.join(', ')}`)
 
 	methods.map(method => {
 		if (!Object.keys(methodObject).includes(method))
-			core.error(`올바르지 않은 METHOD입니다. ${Object.keys(methodObject).join(', ')} 중 하나를 입력해주세요.`)
+			core.error(`${method}는 올바르지 않은 METHOD입니다. ${Object.keys(methodObject).join(', ')} 중 하나를 입력해주세요.`)
 	});
 
 	try {
@@ -41,13 +41,10 @@ async function bootstrap() {
 	} catch (e: unknown) {
 		if (e instanceof Error) {
 			core.error(e.message);
-			core.setFailed('알 수 없는 에러가 발생했습니다.');
 		} else if (typeof e === 'string') {
 			core.error(e);
-			core.setFailed('알 수 없는 에러가 발생했습니다.');
-		} else {
-			core.setFailed('알 수 없는 에러가 발생했습니다.');
 		}
+		core.setFailed('알 수 없는 에러가 발생했습니다.');
 	}
 }
 
